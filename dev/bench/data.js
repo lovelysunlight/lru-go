41ed4f6e8d119aef5c0177a9389eabc2f2b3084e
github-action-benchmark <github@users.noreply.github.com>
2024-08-22 16:55:43 +0000

    add Benchmark (go) benchmark result for c97812b1e4296038256f71eb5f75cb77e9d9d887

---
+++ dev/bench/data.js
@@ -0,0 +1,124 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724345743760,
+  "repoUrl": "https://github.com/lovelysunlight/lru-go",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "jimchen",
+            "email": "jimchen@easyops.cn"
+          },
+          "committer": {
+            "name": "jimchen",
+            "email": "jimchen@easyops.cn"
+          },
+          "id": "c97812b1e4296038256f71eb5f75cb77e9d9d887",
+          "message": "Rename benchmark function",
+          "timestamp": "2024-08-22T16:49:58Z",
+          "url": "https://github.com/lovelysunlight/lru-go/commit/c97812b1e4296038256f71eb5f75cb77e9d9d887"
+        },
+        "date": 1724345742852,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHashicorpLRU_Rand",
+            "value": 193.4,
+            "unit": "ns/op\t      60 B/op\t       0 allocs/op",
+            "extra": "6107355 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Rand - ns/op",
+            "value": 193.4,
+            "unit": "ns/op",
+            "extra": "6107355 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Rand - B/op",
+            "value": 60,
+            "unit": "B/op",
+            "extra": "6107355 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Rand - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "6107355 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Freq",
+            "value": 191.4,
+            "unit": "ns/op\t      55 B/op\t       0 allocs/op",
+            "extra": "6247656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Freq - ns/op",
+            "value": 191.4,
+            "unit": "ns/op",
+            "extra": "6247656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Freq - B/op",
+            "value": 55,
+            "unit": "B/op",
+            "extra": "6247656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHashicorpLRU_Freq - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "6247656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Rand",
+            "value": 198.9,
+            "unit": "ns/op\t      36 B/op\t       0 allocs/op",
+            "extra": "5849656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Rand - ns/op",
+            "value": 198.9,
+            "unit": "ns/op",
+            "extra": "5849656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Rand - B/op",
+            "value": 36,
+            "unit": "B/op",
+            "extra": "5849656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Rand - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "5849656 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Freq",
+            "value": 197.6,
+            "unit": "ns/op\t      33 B/op\t       0 allocs/op",
+            "extra": "6218541 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Freq - ns/op",
+            "value": 197.6,
+            "unit": "ns/op",
+            "extra": "6218541 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Freq - B/op",
+            "value": 33,
+            "unit": "B/op",
+            "extra": "6218541 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkCache_Freq - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "6218541 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
