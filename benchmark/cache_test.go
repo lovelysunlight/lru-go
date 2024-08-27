ba2238569c346d87f2cc174cdee0e5456771207d
JimChenWYU <imjimchen@163.com>
2024-08-27 13:09:57 +0800

    Add benchmark

--- benchmark/cache_test.go
+++ benchmark/cache_test.go
@@ -63,3 +63,117 @@ func BenchmarkCache_Freq(b *testing.B) {
 	}
 	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
 }
+
+func BenchmarkCacheEnbaleLRUK_Rand(b *testing.B) {
+	l, err := lru.New[int64, int64](8192, lru.DisableDeepCopy[int64, int64](), lru.EnableLRUK[int64, int64](2))
+	if err != nil {
+		b.Fatalf("err: %v", err)
+	}
+
+	trace := make([]int64, b.N*2)
+	for i := 0; i < b.N*2; i++ {
+		trace[i] = faker.Int64() % 32768
+	}
+	b.ResetTimer()
+	var hit, miss int
+	for i := 0; i < 2*b.N; i++ {
+		if i%2 == 0 {
+			l.Put(trace[i], trace[i])
+		} else {
+			if _, ok := l.Get(trace[i]); ok {
+				hit++
+			} else {
+				miss++
+			}
+		}
+	}
+	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
+}
+
+func BenchmarkCacheEnbaleLRUK_Freq(b *testing.B) {
+	l, err := lru.New[int64, int64](8192, lru.DisableDeepCopy[int64, int64](), lru.EnableLRUK[int64, int64](2))
+	if err != nil {
+		b.Fatalf("err: %v", err)
+	}
+
+	trace := make([]int64, b.N*2)
+	for i := 0; i < b.N*2; i++ {
+		if i%2 == 0 {
+			trace[i] = faker.Int64() % 16384
+		} else {
+			trace[i] = faker.Int64() % 32768
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Put(trace[i], trace[i])
+	}
+	var hit, miss int
+	for i := 0; i < b.N; i++ {
+		if _, ok := l.Get(trace[i]); ok {
+			hit++
+		} else {
+			miss++
+		}
+	}
+	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
+}
+
+func BenchmarkCacheEnbale2Q_Rand(b *testing.B) {
+	l, err := lru.New[int64, int64](8192, lru.DisableDeepCopy[int64, int64](), lru.Enable2Q[int64, int64](8192*2))
+	if err != nil {
+		b.Fatalf("err: %v", err)
+	}
+
+	trace := make([]int64, b.N*2)
+	for i := 0; i < b.N*2; i++ {
+		trace[i] = faker.Int64() % 32768
+	}
+	b.ResetTimer()
+	var hit, miss int
+	for i := 0; i < 2*b.N; i++ {
+		if i%2 == 0 {
+			l.Put(trace[i], trace[i])
+		} else {
+			if _, ok := l.Get(trace[i]); ok {
+				hit++
+			} else {
+				miss++
+			}
+		}
+	}
+	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
+}
+
+func BenchmarkCacheEnbale2Q_Freq(b *testing.B) {
+	l, err := lru.New[int64, int64](8192, lru.DisableDeepCopy[int64, int64](), lru.Enable2Q[int64, int64](8192*2))
+	if err != nil {
+		b.Fatalf("err: %v", err)
+	}
+
+	trace := make([]int64, b.N*2)
+	for i := 0; i < b.N*2; i++ {
+		if i%2 == 0 {
+			trace[i] = faker.Int64() % 16384
+		} else {
+			trace[i] = faker.Int64() % 32768
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l.Put(trace[i], trace[i])
+	}
+	var hit, miss int
+	for i := 0; i < b.N; i++ {
+		if _, ok := l.Get(trace[i]); ok {
+			hit++
+		} else {
+			miss++
+		}
+	}
+	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
+}
